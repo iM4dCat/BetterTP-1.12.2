93389a65dae2fddc98d4ca36e66eab1f044eb58e
iMadCat <79753838+iM4dCat@users.noreply.github.com>
2024-07-16 16:43:53 +0800

    ..

--- src/main/java/dev/luminous/bettertp/commands/CommandDamage.java
+++ src/main/java/dev/luminous/bettertp/commands/CommandDamage.java
@@ -6,6 +6,7 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
@@ -27,7 +28,7 @@ public class CommandDamage extends CommandBase {
     }
 
     public String getUsage(ICommandSender p_71518_1_) {
-        return "/damage <entity> <amount> or /damage <entity> <amount> <magic/generic/player>";
+        return "/damage <entity> <amount> or /damage <entity> <amount> <magic/generic/[entity]>";
     }
 
     public List<String> getTabCompletions(MinecraftServer p_184883_1_, ICommandSender p_184883_2_, String[] p_184883_3_, @Nullable BlockPos p_184883_4_) {
@@ -42,19 +43,37 @@ public class CommandDamage extends CommandBase {
         if (p_184881_3_.length != 2 && p_184881_3_.length != 3) {
             throw new WrongUsageException(getUsage(null));
         } else {
-            Entity chooseEntity = getEntity(p_184881_1_, p_184881_2_, p_184881_3_[0]);
+            Entity target = getEntity(p_184881_1_, p_184881_2_, p_184881_3_[0]);
             double amount = parseDouble(p_184881_3_[1]);
-            DamageSource damageSource = DamageSource.causePlayerDamage(null);
+            DamageSource damageSource;
             if (p_184881_3_.length == 3) {
                 if (p_184881_3_[2].equals("magic")) {
                     damageSource = DamageSource.MAGIC;
                 } else if (p_184881_3_[2].equals("generic")) {
                     damageSource = DamageSource.GENERIC;
+                } else {
+                    Entity entity = getEntity(p_184881_1_, p_184881_2_, p_184881_3_[2]);
+                    if (entity instanceof EntityPlayer) {
+                        damageSource = DamageSource.causePlayerDamage((EntityPlayer) entity);
+                        if (target instanceof EntityLivingBase && target.world != null) {
+                            target.attackEntityFrom(damageSource, (float) amount);
+                            notifyCommandListener(p_184881_2_, this, "successful", target.getName());
+                        }
+                    } else if (entity instanceof EntityLivingBase) {
+                        damageSource = DamageSource.causeMobDamage((EntityLivingBase) entity);
+                        if (target instanceof EntityLivingBase && target.world != null) {
+                            target.attackEntityFrom(damageSource, (float) amount);
+                            notifyCommandListener(p_184881_2_, this, "successful", target.getName());
+                        }
+                    }
+                    return;
                 }
+            } else {
+                damageSource = DamageSource.causePlayerDamage(null);
             }
-            if (chooseEntity instanceof EntityLivingBase && chooseEntity.world != null) {
-                chooseEntity.attackEntityFrom(damageSource, (float) amount);
-                notifyCommandListener(p_184881_2_, this, "successful", chooseEntity.getName());
+            if (target instanceof EntityLivingBase && target.world != null) {
+                target.attackEntityFrom(damageSource, (float) amount);
+                notifyCommandListener(p_184881_2_, this, "successful", target.getName());
             }
         }
     }
